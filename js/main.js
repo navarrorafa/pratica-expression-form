09924882ed869d47493fef41ede4c485763d8767
ggmj92 <ggmj92@gmail.com>
2023-07-06 18:12:40 +0200

    solucionado ELIGE

--- js/main.js
+++ js/main.js
@@ -4,21 +4,20 @@ let select = document.querySelector('#genero');
 let mostrar = document.querySelector('#mostrarGenero');
 const form = document.querySelector("#form");
 
-let arrayFormulario = []
-
+const arrayFormulario = []
 let arrayGenero = ["terror", "accion", "comedia", "romantica"];
 
-const regExp={
-    titulo:/[a-zA-Z0-9\s]/i,
-    director:/[a-zA-Z\s]/i,
-    anio:/^\d{4}$/,
+const regExp = {
+    titulo: /[a-zA-Z0-9\s]/i,
+    director: /[a-zA-Z\s]/i,
+    anio: /^\d{4}$/,
 }
-form.addEventListener('submit', (ev) =>{
+form.addEventListener('submit', (ev) => {
     ev.preventDefault();
-   validarFormulario()
-        
-    
-    
+    const validado = validarFormulario()
+    if (validado != false) arrayFormulario.push(validado)
+    console.log(validado);
+    console.log(arrayFormulario)
 }
 )
 
@@ -28,6 +27,9 @@ const crearOption = () => {
         select.append(options);
         options.value = item;
         options.innerHTML = item;
+        options.setAttribute('id', `option${index++}`)
+        options.classList.add('opciones')
+
     }
     )
 }
@@ -50,21 +52,18 @@ const recogerDatos = () => {
     const cmpTitulo = document.querySelector("#pelicula").value;
     const cmpDirector = document.querySelector("#director").value;
     const cmpAno = document.querySelector("#ano").value;
-    const cmpGenero = document.querySelector("#option01").value;
-
+    const cmpGenero = document.querySelector('#genero').value;
+    console.log({ cmpGenero });
     const pelicula = {
-        titulo: cmpTitulo, 
-        director: cmpDirector, 
-        ano: cmpAno, 
+        titulo: cmpTitulo,
+        director: cmpDirector,
+        ano: cmpAno,
         genero: cmpGenero
     }
 
-    arrayFormulario.push(pelicula);
+    return pelicula
 
-    form.reset();
-    
 }
-
 
 
 // validar los datos del formulario
@@ -76,53 +75,49 @@ const recogerDatos = () => {
 //     const validarTitulo = titulo => /[a-zA-Z0-9\s]/gi.test(titulo);
 
 //     const validarDirector = director => /[a-zA-Z\s]/gi.test(director);
-    
+
 //     const validarAno = ano => /^\d{4}$/g.test(ano);
-    
+
 //     const validarGenero = () =>  select ==  option01;
 
 //     let arrayValidar = [validarTitulo, validarDirector, validarAno, validarGenero];
 
 //     return arrayValidar;
-   
+
 // }
 
 const validarFormulario = () => {
     // let arrayValidar = reglasValidar();
-    
+
     const titulo = document.querySelector("#pelicula").value;
     const director = document.querySelector("#director").value;
     const anio = document.querySelector("#ano").value;
-    let elige = document.querySelector('#option01').value;
+    let selectElige = document.querySelector('#genero').value;
 
     const anioActual = new Date().getFullYear()
 
     if (!regExp.titulo.test(titulo)) {
         alert("Campo Título Inválido");
-        return ;
-    } 
-    if (!regExp.director.test(director)){
+        return false;
+    }
+    if (!regExp.director.test(director)) {
         alert("Campo Director Inválido");
-        return ;
+        return false;
     }
-    if (!regExp.anio.test(anio) ||  (anio < 1800 || anio > anioActual)){
+    if (!regExp.anio.test(anio) || (anio < 1800 || anio > anioActual)) {
         alert("Campo Año Inválido");
-        return ;
+        return false;
     }
-    if (elige == "elige"){
+    if (selectElige == 'elige') {
         alert("Campo Género Inválido");
-        return ;
+        return false;
+        console.log(selectElige);
     }
 
-
-    recogerDatos();
-
+    const datos = recogerDatos()
+    console.log(datos);
+    return datos
 }
-
-
-
-
-
 
 //almacenar los datos
 
@@ -135,7 +130,6 @@ const validarFormulario = () => {
 
 crearOption();
 crearMostrar();
-// recogerDatos();
-console.log(arrayFormulario)
+
 // console.log(validarFormulario)
-console.log(recogerDatos())
+
